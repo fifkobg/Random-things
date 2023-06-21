cbb7eef77b3f2cef30622e410aa2a0d27ae46078
fifkobg <120708539+fifkobg@users.noreply.github.com>
2023-06-21 08:30:13 +0300

    Add files via upload

---
+++ noidea/noidea/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noidea
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Line LineId { get; set; }
+        public User UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
